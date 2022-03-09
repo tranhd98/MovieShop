e9264a0c2f4b704589eb653c1621e187aec9545a
tranhd98 <tranh9@spu.edu>
2022-03-09 02:25:01 -0800

    changed

---
+++ MovieShopMVC/scripts.sql
@@ -0,0 +1,275 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Genres] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Genres] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220302213238_InitialMigration', N'6.0.2');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [Genres] DROP CONSTRAINT [PK_Genres];
+GO
+
+EXEC sp_rename N'[Genres]', N'Genre';
+GO
+
+DECLARE @var0 sysname;
+SELECT @var0 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Genre]') AND [c].[name] = N'Name');
+IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Genre] DROP CONSTRAINT [' + @var0 + '];');
+ALTER TABLE [Genre] ALTER COLUMN [Name] nvarchar(64) NOT NULL;
+GO
+
+ALTER TABLE [Genre] ADD CONSTRAINT [PK_Genre] PRIMARY KEY ([Id]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220303021821_ChangingGenreTable', N'6.0.2');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Movie] (
+    [Id] int NOT NULL IDENTITY,
+    [Title] nvarchar(256) NULL,
+    [Overview] nvarchar(max) NULL,
+    [Tagline] nvarchar(512) NULL,
+    [Budget] decimal(18,4) NULL DEFAULT 9.9,
+    [Revenue] decimal(18,2) NULL DEFAULT 9.9,
+    [ImdbUrl] nvarchar(2084) NULL,
+    [TmdbUrl] nvarchar(2084) NULL,
+    [PosterUrl] nvarchar(2084) NULL,
+    [BackdropUrl] nvarchar(2084) NULL,
+    [OriginalLanguage] nvarchar(64) NULL,
+    [ReleaseDate] datetime2 NULL,
+    [RunTime] int NULL,
+    [Price] decimal(5,2) NULL DEFAULT 9.9,
+    [CreatedDate] datetime2 NULL DEFAULT (getdate()),
+    [UpdatedDate] datetime2 NULL,
+    [UpdatedBy] nvarchar(max) NULL,
+    [CreatedBy] nvarchar(max) NULL,
+    CONSTRAINT [PK_Movie] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220303034201_CreatingMovieTable', N'6.0.2');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Trailer] (
+    [Id] int NOT NULL IDENTITY,
+    [TrailerUrl] nvarchar(2048) NOT NULL,
+    [Name] nvarchar(256) NOT NULL,
+    [MovieId] int NOT NULL,
+    CONSTRAINT [PK_Trailer] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Trailer_Movie_MovieId] FOREIGN KEY ([MovieId]) REFERENCES [Movie] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_Trailer_MovieId] ON [Trailer] ([MovieId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220303154641_CreatingTrailerTable', N'6.0.2');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [MovieGenre] (
+    [MovieId] int NOT NULL,
+    [GenreId] int NOT NULL,
+    CONSTRAINT [PK_MovieGenre] PRIMARY KEY ([MovieId], [GenreId]),
+    CONSTRAINT [FK_MovieGenre_Genre_GenreId] FOREIGN KEY ([GenreId]) REFERENCES [Genre] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_MovieGenre_Movie_MovieId] FOREIGN KEY ([MovieId]) REFERENCES [Movie] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_MovieGenre_GenreId] ON [MovieGenre] ([GenreId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220303173525_CreatingMovieGenreTable', N'6.0.2');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Cast] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(128) NOT NULL,
+    [Genre] nvarchar(max) NOT NULL,
+    [TmdbUrl] nvarchar(max) NOT NULL,
+    [ProfilePath] nvarchar(2084) NOT NULL,
+    CONSTRAINT [PK_Cast] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [MovieCast] (
+    [MovieId] int NOT NULL,
+    [CastId] int NOT NULL,
+    [Character] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_MovieCast] PRIMARY KEY ([MovieId], [CastId], [Character]),
+    CONSTRAINT [FK_MovieCast_Cast_CastId] FOREIGN KEY ([CastId]) REFERENCES [Cast] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_MovieCast_Movie_MovieId] FOREIGN KEY ([MovieId]) REFERENCES [Movie] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_MovieCast_CastId] ON [MovieCast] ([CastId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220305044747_CreateCastandMovieCastTable', N'6.0.2');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+EXEC sp_rename N'[Cast].[Genre]', N'Gender', N'COLUMN';
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220305045515_CreateCastandMovieCastTableFixed', N'6.0.2');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Role] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(20) NOT NULL,
+    CONSTRAINT [PK_Role] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [User] (
+    [Id] int NOT NULL IDENTITY,
+    [FirstName] nvarchar(128) NULL,
+    [LastName] nvarchar(128) NULL,
+    [DateOfBirth] datetime2 NULL,
+    [Email] nvarchar(256) NULL,
+    [HashedPassword] nvarchar(1024) NULL,
+    [Salt] nvarchar(1024) NULL,
+    [PhoneNumber] nvarchar(16) NULL,
+    [TwoFactorEnabled] bit NULL,
+    [LockoutEndDate] datetime2 NULL,
+    [LastLoginDateTime] datetime2 NULL,
+    [IsLocked] bit NULL,
+    [AccessFailedCount] int NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Favorite] (
+    [Id] int NOT NULL IDENTITY,
+    [MovieId] int NOT NULL,
+    [UserId] int NOT NULL,
+    CONSTRAINT [PK_Favorite] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Favorite_Movie_MovieId] FOREIGN KEY ([MovieId]) REFERENCES [Movie] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Favorite_User_UserId] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [Purchase] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] int NOT NULL,
+    [PurchaseNumber] uniqueidentifier NULL,
+    [TotalPrice] decimal(18,2) NULL DEFAULT 9.9,
+    [PurchaseDateTime] datetime2 NULL DEFAULT (getdate()),
+    [MovieId] int NOT NULL,
+    CONSTRAINT [PK_Purchase] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Purchase_Movie_MovieId] FOREIGN KEY ([MovieId]) REFERENCES [Movie] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Purchase_User_UserId] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [Review] (
+    [MovieId] int NOT NULL,
+    [UserId] int NOT NULL,
+    [Rating] decimal(3,2) NOT NULL DEFAULT 9.9,
+    [ReviewText] nvarchar(2084) NOT NULL,
+    CONSTRAINT [PK_Review] PRIMARY KEY ([MovieId], [UserId]),
+    CONSTRAINT [FK_Review_Movie_MovieId] FOREIGN KEY ([MovieId]) REFERENCES [Movie] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Review_User_UserId] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [UserRole] (
+    [UserId] int NOT NULL,
+    [RoleId] int NOT NULL,
+    CONSTRAINT [PK_UserRole] PRIMARY KEY ([UserId], [RoleId]),
+    CONSTRAINT [FK_UserRole_Role_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [Role] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_UserRole_User_UserId] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_Favorite_MovieId] ON [Favorite] ([MovieId]);
+GO
+
+CREATE INDEX [IX_Favorite_UserId] ON [Favorite] ([UserId]);
+GO
+
+CREATE INDEX [IX_Purchase_MovieId] ON [Purchase] ([MovieId]);
+GO
+
+CREATE INDEX [IX_Purchase_UserId] ON [Purchase] ([UserId]);
+GO
+
+CREATE INDEX [IX_Review_UserId] ON [Review] ([UserId]);
+GO
+
+CREATE INDEX [IX_UserRole_RoleId] ON [UserRole] ([RoleId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220307071024_createUserAndReviewAndFavoriteAndPurchaseAndRoleAndUserRoleTable', N'6.0.2');
+GO
+
+COMMIT;
+GO
+
